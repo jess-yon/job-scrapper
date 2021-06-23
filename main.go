c4fc30c5d5ca9f7b608ee15977080844e596897e
Yeonju Lee <jessy2yon@gmail.com>
2021-06-24 06:42:10 +0900

    putting data into one array

--- main.go
+++ main.go
@@ -21,16 +21,25 @@ type extractedJob struct {
 var baseURL string = "https://kr.indeed.com/jobs?q=devops&limit=50"
 
 func main() {
-	totalPages := getPages() // for문의 범위를 구함
+	var jobs []extractedJob   // jobs는 extractedJob을 요소로 하는 배열
+
+	totalPages := getPages() // for문의 범위(length)를 구함
 	
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)  //? getting all the jobs
+		
+		jobs = append(jobs, extractedJobs...)
+		//? To append the CONTENTS of extractedJobs, simply add '...' => similar to 'Spread Syntax' in JS
 	}
+
+	fmt.Println(jobs)
+	//! => this main func is the combination of many arrays
 }
 
 
-// 페이지 별 상세내용을 가져오는 함수
-func getPage(page int) {
+//! 페이지 별 상세내용을 가져오는 함수
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob   // jobs는 extractedJob을 요소로 하는 배열
 	pageURL := baseURL + "&start=" + strconv.Itoa(page * 50) // strconv.Itoa() 는 number => string으로 변환
 	fmt.Println(pageURL)
 
@@ -42,28 +51,47 @@ func getPage(page int) {
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)  // doc은 불러온 html document
 
-	//! Find method => 'jobsearch-SerpJobCard' 이라는 className을 가진 태그를 가져온다
+	//? Find method => 'jobsearch-SerpJobCard' 이라는 className을 가진 태그를 가져온다
 	searchCards := doc.Find(".jobsearch-SerpJobCard")
-	searchCards.Each(func(i int, card *goquery.Selection) {
-		id, _ := card.Attr("data-jk")   //? 'Attr' method는 값, 존재여부를 리턴
-		title := cleanString(card.Find(".title > a").Text())  // title class 안의 a 태그를 찾음 => text로 변환		
-		location := cleanString(card.Find(".sjcl").Text())
 
-		fmt.Println(id, title, location)
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)  // jobs라는 배열에 job이라는 변수(extractJob(card))를 하나씩 넣어준다
 	})
+
+	return jobs  // job을 모아둔 배열을 return
 }
 
 
-// 공백을 제거해서 한 줄의 string으로 만들어 주는 함수 (strings 패키지 이용)
+//! Job 하나를 추출하는 함수
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")   //? 'Attr' method는 값, 존재여부를 리턴
+	title := cleanString(card.Find(".title > a").Text())  // title class 안의 a 태그를 찾음 => text로 변환		
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+
+	return extractedJob {
+		id: id, 
+		title: title, 
+		location: location, 
+		salary: salary, 
+		summary: summary,
+	}
+	//fmt.Println(id, title, location, salary, summary)
+}
+
+
+//! 공백을 제거해서 한 줄의 string으로 만들어 주는 함수 (strings 패키지 이용)
 func cleanString(str string) string {
-	// TrimSpace로 양쪽에 공백을 없애 줌
-	// -> Fields로 하나의 배열로 만들어 줌
-	// -> Join으로 다시 합쳐줌
+	//? TrimSpace로 양쪽에 공백을 없애 줌
+	//? -> Fields로 하나의 배열로 만들어 줌
+	//? -> Join으로 다시 합쳐줌
 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 
-// 페이지가 총 몇개인지 구하는 함수
+//! 페이지가 총 몇개인지 구하는 함수
 func getPages() int {
 	pages := 0
 
@@ -86,7 +114,7 @@ func getPages() int {
 }
 
 
-// 에러 여부 체크
+//! 에러 여부 체크
 func checkErr(err error) {
 	if err != nil {
 		log.Fatalln(err)
@@ -94,7 +122,7 @@ func checkErr(err error) {
 }
 
 
-// 응답 코드 체크
+//! 응답 코드 체크
 func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status: ", res.StatusCode)
