f345012f70f1b83f1bca382e02b26ab9ff976c63
Yeonju Lee <jessy2yon@gmail.com>
2021-06-24 04:32:56 +0900

    setting each page url

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -10,10 +12,24 @@ import (
 var baseURL string = "https://kr.indeed.com/jobs?q=devops&limit=50"
 
 func main() {
-	getPages()
+	totalPages := getPages() // for문의 범위를 구함
+	
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
 }
 
+
+// 페이지 별 상세내용을 가져오는 함수
+func getPage(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page * 50) // strconv.Itoa() 는 number => string으로 변환
+	fmt.Println(pageURL)
+}
+
+
+// 페이지가 총 몇개인지 구하는 함수
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
@@ -23,11 +39,14 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)  // doc은 불러온 html document
 	checkErr(err)
 
-	doc.Find("./pagination")  // pagination 이라는 className을 가진 태그를 가져온다
-	
-	// fmt.Println(doc)
-	return 0  // (temporary return value)
+	//! Find method => 'pagination' 이라는 className을 가진 태그를 가져온다
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()  // <a> tag 가 몇개인지
+	})  
+
+	return pages
 }
+
 
 // 에러 여부 체크
 func checkErr(err error) {
@@ -35,6 +54,7 @@ func checkErr(err error) {
 		log.Fatalln(err)
 	}
 }
+
 
 // 응답 코드 체크
 func checkCode(res *http.Response) {
