f39d827cf034c3599820782d125e17bf350836d6
Yeonju Lee <jessy2yon@gmail.com>
2021-06-25 21:11:28 +0900

    sending post request to server

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
-	"github.com/jess-yon/job-scrapper/scrapper"
+	"job-scrapper/scrapper"
+	"os"
+	"strings"
+
 	"github.com/labstack/echo/v4"
 )
 
+const fileName string = "jobs.csv"
+
 func handleHome(c echo.Context) error {
-	// return c.String(http.StatusOK, "Hello, World!") //? 문자열을 전달
+	// return c.String(http.StatusOK, "Hello, World!") //? 문자열 전달
 	return c.File("home.html")  //?  file 전달
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)  // handleScrape func를 통해 사용자에게 file 보낸 후, 서버에서는 파일을 remove
+
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	
+	// 첨부파일을 return 해주는 기능 (저장된 파일 이름, 전달할 파일 이름)
+	return c.Attachment(fileName, fileName)  //! => 'job.scv'라는 파일이 다운로드 됨
 }
 
 func main() {
 	e := echo.New()
 	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
 	e.Logger.Fatal(e.Start(":1323"))  // server on port 1323
-
-	scrapper.Scrape("term")
 }
